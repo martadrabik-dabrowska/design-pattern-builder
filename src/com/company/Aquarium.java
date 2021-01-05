714768c7cf951c2c5686d54531fddff74ea37d51
Marta Drabik-Dabrowska <martad1388@gmail.com>
2021-01-05 12:58:00 +0100

    Add new files

---
+++ src/com/company/Aquarium.java
@@ -0,0 +1,79 @@
+package com.company;
+
+public class Aquarium {
+    private String walls;
+    private String sand;
+    private String plants;
+    private String fish;
+    private String water;
+
+
+    private Aquarium(AquariumBuilder aquariumBuilder){
+        this.walls = aquariumBuilder.walls;
+        this.sand = aquariumBuilder.sand;
+        this.plants = aquariumBuilder.plants;
+        this.fish = aquariumBuilder.fish;
+        this.water = aquariumBuilder.water;
+    }
+
+    public String getWalls(){
+        return walls;
+    }
+
+    public String getSand() {
+        return sand;
+    }
+
+    public String getPlants() {
+        return plants;
+    }
+
+    public String getFish() {
+        return fish;
+    }
+    public String getWater(){
+        return water;
+    }
+
+
+ public static class AquariumBuilder{
+     private String walls;
+     private String sand;
+     private String plants;
+     private String fish;
+     private String water;
+
+     public AquariumBuilder buildWalls(String walls){
+         this.walls = walls;
+         return this;
+     }
+     public AquariumBuilder buildSand(String sand){
+         this.sand = sand;
+         return this;
+     }
+     public AquariumBuilder buildPlants(String plants){
+         this.plants = plants;
+         return this;
+     }
+     public AquariumBuilder buildFish(String fish){
+         this.fish = fish;
+         return this;
+     }
+     public AquariumBuilder buildWater(String water){
+         this.water = water;
+         return this;
+     }
+
+     public Aquarium build(){
+         return new Aquarium(this);
+     }
+
+
+
+ }
+
+
+
+
+
+}
