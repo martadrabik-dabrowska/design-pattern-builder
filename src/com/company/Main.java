66a7560e7ce5b6a3eb16cf632995bf5e2894589e
martadrabik-dabrowska <46379532+martadrabik-dabrowska@users.noreply.github.com>
2021-01-07 12:06:05 +0100

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,13 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	Aquarium aquarium = new Aquarium.AquariumBuilder()
+            .buildWalls("walls")
+            .buildSand("yellow sand")
+            .buildPlants("small plants")
+            .buildWater("30 litres of water")
+            .buildFish("golden fishes")
+            .build();
+	System.out.println(aquarium);
     }
 }
